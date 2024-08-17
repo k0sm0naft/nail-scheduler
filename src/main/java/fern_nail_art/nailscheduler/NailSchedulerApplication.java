6684d7322f01636577f2355129baf53f3e3dcbbf
Artem Akymenko <akimenko.artem@gmail.com>
2024-08-17 15:00:32 +0300

    Initial commit

---
+++ src/main/java/fern_nail_art/nailscheduler/NailSchedulerApplication.java
@@ -0,0 +1,13 @@
+package fern_nail_art.nailscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NailSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NailSchedulerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fern_nail_art/nailscheduler/NailSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package fern_nail_art.nailscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NailSchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
